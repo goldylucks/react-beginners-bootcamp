b6ff008f792f27ec324ad403d27cf673ae935d6b
goldylucks <adamgoldman7@gmail.com>
2017-04-30 20:22:27 +0800

    (module-1) step 11: class components and initial state

--- src/client/Flip.jsx
+++ src/client/Flip.jsx
@@ -12,35 +12,42 @@ const styles = {
   },
 }
 
-const FlipApp = () =>
-  <div className="container app-container">
-    <h2>Flip</h2>
-    <div className="row">
-      <div className="col-md-4">
-        <h3>Controls</h3>
+class FlipApp extends React.Component {
+  state = {
+    score: 0,
+    fillResult: null,
+    guessed: null,
+  }
+
+  render() {
+    return (
+      <div className="container app-container">
+        <h2>Flip</h2>
         <div className="row">
           <div className="col-md-4">
-            <button className="btn btn-primary btn-block">Heads</button>
+            <h3>Controls</h3>
+            <div className="row">
+              <div className="col-md-4">
+                <button className="btn btn-primary btn-block">Heads</button>
+              </div>
+              <div className="col-md-4">
+                <button className="btn btn-primary btn-block">Tails</button>
+              </div>
+              <div className="col-md-4">
+                <button className="btn btn-default btn-block">Reset</button>
+              </div>
+            </div>
           </div>
-          <div className="col-md-4">
-            <button className="btn btn-primary btn-block">Tails</button>
-          </div>
-          <div className="col-md-4">
-            <button className="btn btn-default btn-block">Reset</button>
+          <div className="col-md-2 col-md-offset-2">
+            <h3>Output</h3>
+            <div className="well">
+              <span style={styles.score}>Score: <strong>{this.state.score}</strong></span>
+            </div>
           </div>
         </div>
       </div>
-      <div className="col-md-2 col-md-offset-2">
-        <h3>Output</h3>
-        <div className="well">
-          <span style={styles.score}>Score: <strong>0</strong></span>
-          <div style={styles.success}>Heads: you win!</div>
-          <div style={styles.failure}>Heads: you lose!</div>
-          <div style={styles.success}>Tails: you win!</div>
-          <div style={styles.failure}>Tails: you lose!</div>
-        </div>
-      </div>
-    </div>
-  </div>
+    )
+  }
+}
 
 export default FlipApp
