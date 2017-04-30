a6fece8625dd6c4e6f36cebfd5ec5b5047f739d2
goldylucks <adamgoldman7@gmail.com>
2017-04-30 18:44:09 +0800

    (module-1) step2: scaffold (Rest)

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -5,3 +5,23 @@ ReactDOM.render(
   <h2>Flip</h2>,
   document.getElementById('flip-app'),
 )
+
+ReactDOM.render(
+  <h2>Guess Number</h2>,
+  document.getElementById('guessnumber-app'),
+)
+
+ReactDOM.render(
+  <h2>Fizzbuzz</h2>,
+  document.getElementById('fizzbuzz-app'),
+)
+
+ReactDOM.render(
+  <h2>Auth</h2>,
+  document.getElementById('auth-app'),
+)
+
+ReactDOM.render(
+  <h2>Series</h2>,
+  document.getElementById('series-app'),
+)
