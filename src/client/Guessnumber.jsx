1ae7183b94ba009633b2d7c60099857fed8a4f3b
goldylucks <adamgoldman7@gmail.com>
2017-06-11 08:26:08 +0900

    mock ui (rest)

--- src/client/Guessnumber.jsx
+++ src/client/Guessnumber.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 
 import App from './App'
 
+const styles = {
+  success: {
+    color: 'limegreen ',
+  },
+  failure: {
+    color: 'red',
+  },
+}
+
 const Guessnumber = () => (
-  <App title="Guessnumber" />
+  <App
+    title="Guessnumber"
+    controls={
+      <div className="input-group">
+        <input className="form-control" type="number" placeholder="Place your guess here" min={1} max={100} />
+        <span className="input-group-btn">
+          <button className="btn btn-primary">Guess</button>
+        </span>
+      </div>
+    }
+    output={
+      <div>
+        <div style={styles.failure}>Too low ...</div>
+        <div style={styles.failure}>Too high ...</div>
+        <div style={styles.success}>Success! The number is 73</div>
+      </div>
+    }
+  />
 )
 
 export default Guessnumber
