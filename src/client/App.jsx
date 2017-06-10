e7775ceb400a791654ed1ac331423d8036d34df9
goldylucks <adamgoldman7@gmail.com>
2017-06-11 08:13:03 +0900

    mock UI (flip)

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 
 type Props = {
   title: string,
+  controls: Object,
+  output: ?Object,
 }
 
-const App = ({ title }: Props) => (
-  <h1>{ title }</h1>
+const App = ({ title, controls, output }: Props) => (
+  <div className="container app-container">
+    <h1>{ title }</h1>
+    <div className="row">
+      <div className="col-md-4">
+        <h3>Controls</h3>
+        {controls}
+      </div>
+      <div className="col-md-2 col-md-offset-2">
+        <h3>Output</h3>
+        <div className="well">
+          {output}
+        </div>
+      </div>
+    </div>
+  </div>
 )
 
 export default App
--- src/client/Flip.jsx
+++ src/client/Flip.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 
 import App from './App'
 
+const styles = {
+  success: {
+    color: 'limegreen',
+  },
+  failure: {
+    color: 'red',
+  },
+}
+
 const Flip = () => (
-  <App title="Flip" />
+  <App
+    title="Flip"
+    controls={
+      <div className="row">
+        <div className="col-md-4">
+          <button className="btn btn-primary btn-block">Heads</button>
+        </div>
+        <div className="col-md-4">
+          <button className="btn btn-primary btn-block">Tails</button>
+        </div>
+        <div className="col-md-4">
+          <button className="btn btn-default btn-block">Reset</button>
+        </div>
+      </div>
+    }
+    output={
+      <div>
+        <span style={{ color: 'green' }}>Score: <strong>0</strong></span>
+        <div style={styles.success}>Heads: you win!</div>
+        <div style={styles.fialure}>Heads: you lose!</div>
+        <div style={styles.success}>Tails: you win!</div>
+        <div style={styles.fialure}>Tails: you lose!</div>
+      </div>
+    }
+  />
 )
 
 export default Flip
