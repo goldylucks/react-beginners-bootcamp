2ed174b5a1361dbf119a037fa2157a31cf94690b
goldylucks <adamgoldman7@gmail.com>
2017-06-11 08:41:52 +0900

    class components and initial state (flip)

--- src/client/Flip.jsx
+++ src/client/Flip.jsx
@@ -11,32 +11,40 @@ const styles = {
   },
 }
 
-const Flip = () => (
-  <App
-    title="Flip"
-    controls={
-      <div className="row">
-        <div className="col-md-4">
-          <button className="btn btn-primary btn-block">Heads</button>
-        </div>
-        <div className="col-md-4">
-          <button className="btn btn-primary btn-block">Tails</button>
-        </div>
-        <div className="col-md-4">
-          <button className="btn btn-default btn-block">Reset</button>
-        </div>
-      </div>
-    }
-    output={
-      <div>
-        <span style={{ color: 'green' }}>Score: <strong>0</strong></span>
-        <div style={styles.success}>Heads: you win!</div>
-        <div style={styles.fialure}>Heads: you lose!</div>
-        <div style={styles.success}>Tails: you win!</div>
-        <div style={styles.fialure}>Tails: you lose!</div>
-      </div>
-    }
-  />
-)
+class Flip extends React.Component {
+  state = {
+    score: 0,
+  }
+
+  render() {
+    return (
+      <App
+        title="Flip"
+        controls={
+          <div className="row">
+            <div className="col-md-4">
+              <button className="btn btn-primary btn-block">Heads</button>
+            </div>
+            <div className="col-md-4">
+              <button className="btn btn-primary btn-block">Tails</button>
+            </div>
+            <div className="col-md-4">
+              <button className="btn btn-default btn-block">Reset</button>
+            </div>
+          </div>
+        }
+        output={
+          <div>
+            <span style={{ color: 'green' }}>Score: <strong>{this.state.score}</strong></span>
+            <div style={styles.success}>Heads: you win!</div>
+            <div style={styles.fialure}>Heads: you lose!</div>
+            <div style={styles.success}>Tails: you win!</div>
+            <div style={styles.fialure}>Tails: you lose!</div>
+          </div>
+        }
+      />
+    )
+  }
+}
 
 export default Flip
