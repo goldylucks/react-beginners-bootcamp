a47e9d2c23951ad2f2ab55d436f690a64db2ce02
goldylucks <adamgoldman7@gmail.com>
2017-06-28 12:33:06 +0800

    UI (rest)

--- src/client/Auth.jsx
+++ src/client/Auth.jsx
@@ -5,6 +5,42 @@ import React from 'react'
 import App from './App'
 
 const Auth = () =>
-  <App title="Auth" />
+  <App
+    title="Auth"
+    controls={
+      <div>
+        <div className="row">
+          <div className="col-md-12 form-group">
+            <input placeholder="Name" className="form-control" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 form-group">
+            <input type="email" placeholder="Email" className="form-control" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 form-group">
+            <input type="password" placeholder="Password" className="form-control" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <button className="btn btn-primary btn-block">Signup</button>
+          </div>
+          <div className="col-md-6">
+            <button className="btn btn-default btn-block">Login</button>
+          </div>
+        </div>
+      </div>
+    }
+    output={
+      <div>
+        <p>Hello guest</p>
+        <p>Welcome back Spidy</p>
+        <p>Welcome aboard Spidy!</p>
+      </div>
+    }
+  />
 
 export default Auth
--- src/client/client.jsx
+++ src/client/client.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Welcome from './Welcome'
 import Flip from './Flip'
