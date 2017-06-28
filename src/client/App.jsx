d6266ac8fea7ca65a351530ccd72fdfac9ea733d
goldylucks <adamgoldman7@gmail.com>
2017-06-28 12:21:35 +0800

    ui (Flip)

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
 
-const App = ({ title }) =>
-  <h1>{title} modular</h1>
+const App = ({ title, controls, output }) =>
+  <div className="container app-container">
+    <h2>{ title }</h2>
+    <div className="row">
+      <div className="col-md-4">
+        <h3>Controls</h3>
+        <div className="row">
+          {controls}
+        </div>
+      </div>
+      <div className="col-md-2 col-md-offset-2">
+        <h3>Output</h3>
+        <div className="well">
+          {output}
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 export default App
