06f1d85b1d4694af22620ab721903cd457b03280
goldylucks <adamgoldman7@gmail.com>
2017-06-29 10:08:48 +0800

    nav rest

--- src/client/Nav.jsx
+++ src/client/Nav.jsx
@@ -10,6 +10,9 @@ const Nav = () =>
       <ul className="nav navbar-nav">
         <li><NavLink to="/" className="nav-item">Home</NavLink></li>
         <li><NavLink to="/flip" className="nav-item">Flip</NavLink></li>
+        <li><NavLink to="/guessnumber" className="nav-item">Guessnumber</NavLink></li>
+        <li><NavLink to="/fizzbuzz" className="nav-item">Fizzbuzz</NavLink></li>
+        <li><NavLink to="/auth" className="nav-item">Auth</NavLink></li>
       </ul>
     </div>
   </nav>
